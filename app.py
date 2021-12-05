91911a8f268d5b95e1c683677ef6f195db5888a9
Kranthi Lakum <krathi.lakum@qvantel.com>
2021-12-05 13:08:08 +0530

    Add timestamp to `page.html`

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -12,7 +13,13 @@ def cakes():
 
 @app.route('/hello/<name>')
 def greet(name):
-  return render_template('page.html', name=name)
+  now = datetime.datetime.now() # get current time and store it in `now`
+  timeString = now.strftime('%d-%m-%Y %H:%M:%S') # create formatted stringn of date and time from `now` object
+  templateData = { # create dictionary of keys and associated values (variables)
+    'title': name,
+    'time': timeString
+  }
+  return render_template('page.html', **templateData) # pass dictionary to template
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
